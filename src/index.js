5bf14d5eb9bebb403f72ceb5c4567dfd9674b4f9
Oleh Ismailov <oleh.ismailov@nordsec.com>
2021-05-28 19:08:43 +0300

    sso with asure

--- src/index.js
+++ src/index.js
@@ -3,15 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { configSSO } from './sso-config.js'
+
+const config = {
+    auth: {
+        clientId: configSSO.clientId,
+        clientSecret: configSSO.clientSecret,
+        redirectUri: configSSO.redirectUri,
+        scopes: configSSO.scopes,
+    }
+};
+
+const publicClientApplication = new PublicClientApplication(config);
+
+publicClientApplication.loginPopup()
+    .then(function (loginResponse) {
+        // successful login. You can fetch user data here using loginResponse object
+        console.log("accountId:", loginResponse.account.homeAccountId)
+        console.log("username:", loginResponse.account.name)
+        console.log("token:", loginResponse.accessToken)
+
+    }).catch(function (error) {
+        console.log("This is an error", error);
+    });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <MsalProvider instance={publicClientApplication}>
+            <App />
+        </ MsalProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
---
+++ src/sso-config.js
@@ -0,0 +1,8 @@
+export const configSSO = {
+    clientId: '6337fcbd-c418-4967-8621-a10146d5acc6',
+    clientSecret: '3e74e520-6388-4969-a4dd-f36efb05285d',
+    redirectUri: "http://localhost:3000/",
+    scopes: [
+        'user.read'
+    ],
+};
